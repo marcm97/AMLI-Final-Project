a451e4257f5c24aa05a06467bb6ec28322d39834
Marc Mascarenhas <marc.mascarenhas@mines.sdsmt.edu>
2019-07-30 14:43:12 -0700

    c

--- APP/app.py
+++ APP/app.py
@@ -19,22 +19,19 @@ import requests
 import json
 import cv2 as cv
 
+# for using pickled files and creating graphs
 import pickle
 import matplotlib.pyplot as plt
-
-
-
-
 
 
 # create the application object
 app = Flask(__name__)
 app.config.from_object(Config)
-app.config['GOOGLEMAPS_KEY'] = "AIzaSyDtYp6V7WFFzm5JpAGBYwIXnayqN4E-Z1Q"
+app.config['GOOGLEMAPS_KEY'] = ""
 GoogleMaps(app)
 
 #yelp api key
-api_key="CjSlZf6-T83KQTmcssS499VYq6bzAICqtwPfk0zz7tFOtBfV1uNROedPiXJaCduFfBAHAfKKijOWtwUnBnI5_am4eBdE3tPqDVJ0NiTAOBAnFrs2cp967NQWfpBrW3Yx"
+api_key=""
         
 
 @app.route('/', methods=['GET', 'POST'])
@@ -42,14 +39,13 @@ api_key="CjSlZf6-T83KQTmcssS499VYq6bzAICqtwPfk0zz7tFOtBfV1uNROedPiXJaCduFfBAHAfK
 def home():
     return render_template('home.html')
 
-@app.route('/about', methods=['GET', 'POST'])
+@app.route('/about')
 def about():
     return render_template('about.html')
 
 @app.route('/vis/test/<id>', methods=['GET', 'POST'])
 def vis(id):
     return render_template('vis.html',id = id)
-
 
 @app.route('/<destination>/<restaurant>')
 def restaurant_reviews(restaurant,destination):
@@ -75,7 +71,11 @@ def restaurant_reviews(restaurant,destination):
     phone = req.json()["display_phone"]
     image = requests.get(req.json()["image_url"])
     yelp_website = req.json()["url"]
+        
+    with open("static/images_" + id +".jpg", 'wb') as f:
+        f.write(image.content)
     
+    #creating visualization of topic distribution for query
     lda = pickle.load(open("pickled/" + destination + "_lda.pkl","rb"))
     dictionary = pickle.load(open("pickled/" + destination + "_dictionary.pkl","rb"))
     new_bow = dictionary.doc2bow(eval(data.iloc[0,-3]))
@@ -83,28 +83,19 @@ def restaurant_reviews(restaurant,destination):
     labels = ['topic1','topic2','topic3','topic4','topic5','topic6']
     angle=np.linspace(0, 2*np.pi, len(labels), endpoint=False)
 
-    vals = new_doc_distribution
 
-    stats=np.concatenate((vals,[vals[0]]))
+    stats=np.concatenate((new_doc_distribution,[new_doc_distribution[0]]))
     angles=np.concatenate((angle,[angle[0]]))
     plt.polar(angles,stats)
-    # for i in range(vals):
-    #     plt.text(0.05,0.95-i,vals[i])
     plt.savefig('static/'+ id+'.png')
     plt.show()
 
-    
-    
-
-    with open("static/images_" + id +".jpg", 'wb') as f:
-        f.write(image.content)
-
     return render_template('restaurant.html',restaurant = restaurant, destination = destination,negative = negative,positive = positive,stars = stars, id = id, phone = phone,graph = 'graph_'+id+".png" ,image ='images_'+id+".jpg",yelp_website = yelp_website)
-
 
 @app.route("/map", methods=['POST', 'GET'])
 def get_information():
-    
+    #Takes in information from form, 
+    #Outputs Map, along with hyperlinks of top 10 similar restaurants
     form = ReusableForm(request.form)
     print (form.errors)
 
@@ -115,7 +106,6 @@ def get_information():
             departure=request.form['departure']
             destination=request.form['destination']
             restaurant=request.form['restaurant']
-            
             query = pd.read_csv("csv/" + departure +'.csv')
             query = query[query["name"] == restaurant]
 
@@ -124,9 +114,9 @@ def get_information():
                 address = query.address.values
                 #let user select which address is being referred to
                 return render_template('address.html', restaurant = restaurant, address = address, departure = departure, destination =destination )
+            
             else:
                 most_similar_df = get_top_ten(query,departure,destination)
-
                 names =  most_similar_df.name.tolist()
                 latitudes = most_similar_df.latitude.tolist()
                 longitudes = most_similar_df.longitude.tolist()
@@ -145,12 +135,9 @@ def get_information():
         except:
             return render_template('error.html')
 
-
 @app.route('/map2', methods=['POST', 'GET'])
-
 def get_information2():
-
-
+    #If more than 2 locations with same name
     if request.method == 'POST':
         address=request.form['address']
         departure = request.form['departure']
