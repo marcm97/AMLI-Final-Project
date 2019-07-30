0a0a6d381c8c9a42f12fccc33dcb70e2c54c4504
Marc Mascarenhas <marc.mascarenhas@mines.sdsmt.edu>
2019-07-30 11:20:05 -0700

    updated app

--- APP/app.py
+++ APP/app.py
@@ -1,180 +1,193 @@
-<<<<<<< HEAD
+##Libraries
 from flask import Flask, render_template, flash, request, jsonify
 from wtforms import Form, TextField, TextAreaField, validators, StringField, SubmitField
 from config import Config
 from forms import ReusableForm
 
+#csv/df manipulations
 import pandas as pd
 import numpy as np
 
+#maps
 from flask_googlemaps import GoogleMaps,Map
 
+#custom functions from functions.py
 from functions import jensen_shannon,get_most_similar_documents,get_top_ten
+
+#libraries for rendering images
+import requests
+import json
+import cv2 as cv
+
+import pickle
+import matplotlib.pyplot as plt
+
+
+
+
 
 
 # create the application object
 app = Flask(__name__)
 app.config.from_object(Config)
-app.config['GOOGLEMAPS_KEY'] = "key"
+app.config['GOOGLEMAPS_KEY'] = ""
 GoogleMaps(app)
+
+#yelp api key
+api_key=""
         
 
 @app.route('/', methods=['GET', 'POST'])
+@app.route('/home', methods=['GET', 'POST'])
 def home():
-    return render_template('base.html')
+    return render_template('home.html')
 
-@app.route('/<destination>/<some_place>')
-def home2(some_place,destination):
-    data = pd.read_csv("csv/reviews" + "_" + destination + ".csv")
-    text = data[data.name ==some_place].text.values
+@app.route('/about', methods=['GET', 'POST'])
+def about():
+    return render_template('about.html')
 
-    return render_template('base2.html',place = some_place,place2 = destination,text=text)
+@app.route('/vis/test/<id>', methods=['GET', 'POST'])
+def vis(id):
+    return render_template('vis.html',id = id)
+
+
+@app.route('/<destination>/<restaurant>')
+def restaurant_reviews(restaurant,destination):
+    #input: restaurant, destination
+    #output: passes info to restaurant.html
+    #info obtained from yelp api + cleaned csv files
+    data = pd.read_csv("csv/"+ destination + ".csv")
+    data = data[data.name ==restaurant].reset_index(drop=True)
+    id = data.loc[0,"business_id"]
+    
+    #getting top 5 positive/negative sentimemts
+    positive = eval(data.loc[0,"postive_reviews"])[:5]
+    negative = eval(data.loc[0,"negative_reviews"])[:5]
+    
+    #stars
+    stars = data.loc[0,"stars"]
+
+    #yelp api call
+    headers = {'Authorization': 'Bearer %s' % api_key}
+    url='https://api.yelp.com/v3/businesses/' + id
+    req=requests.get(url, params={'location':destination}, headers=headers)
+    
+    phone = req.json()["display_phone"]
+    image = requests.get(req.json()["image_url"])
+    yelp_website = req.json()["url"]
+    
+    lda = pickle.load(open("pickled/" + destination + "_lda.pkl","rb"))
+    dictionary = pickle.load(open("pickled/" + destination + "_dictionary.pkl","rb"))
+    new_bow = dictionary.doc2bow(eval(data.iloc[0,-3]))
+    new_doc_distribution = np.array([tup[1] for tup in lda.get_document_topics(bow=new_bow)])
+    labels = ['topic1','topic2','topic3','topic4','topic5','topic6']
+    angle=np.linspace(0, 2*np.pi, len(labels), endpoint=False)
+
+    vals = new_doc_distribution
+
+    stats=np.concatenate((vals,[vals[0]]))
+    angles=np.concatenate((angle,[angle[0]]))
+    plt.polar(angles,stats)
+    # for i in range(vals):
+    #     plt.text(0.05,0.95-i,vals[i])
+    plt.savefig('static/'+ id+'.png')
+    plt.show()
+
+    
+    
+
+    with open("static/images_" + id +".jpg", 'wb') as f:
+        f.write(image.content)
+
+    return render_template('restaurant.html',restaurant = restaurant, destination = destination,negative = negative,positive = positive,stars = stars, id = id, phone = phone,graph = 'graph_'+id+".png" ,image ='images_'+id+".jpg",yelp_website = yelp_website)
 
 
 @app.route("/map", methods=['POST', 'GET'])
 def get_information():
+    
     form = ReusableForm(request.form)
     print (form.errors)
 
 
     if request.method == 'POST':
-        try:
-            departure=request.form['departure']
-            destination=request.form['destination']
-            restaurant=request.form['restaurant']
 
-            most_similar_df = get_top_ten(departure,destination,restaurant)
-            latitudes = most_similar_df.latitude.tolist()
-            names =  most_similar_df.name.tolist()
+        # try:
         
-            
+    
+
+        departure=request.form['departure']
+        destination=request.form['destination']
+        restaurant=request.form['restaurant']
+        
+        query = pd.read_csv("csv/" + departure +'.csv')
+        query = query[query["name"] == restaurant]
+
+        # if more than one business has the same name
+        if query.shape[0]>1:
+            address = query.address.values
+            #let user select which address is being referred to
+            return render_template('address.html', restaurant = restaurant, address = address, departure = departure, destination =destination )
+        else:
+            most_similar_df = get_top_ten(query,departure,destination)
+
+            names =  most_similar_df.name.tolist()
+            latitudes = most_similar_df.latitude.tolist()
             longitudes = most_similar_df.longitude.tolist()
             locations = list(zip(latitudes,longitudes,names))
             
-        
             mymap = Map(
-            identifier="view-side",
-            lat=33.4227198,
-            lng= -111.79821229999999,
-            markers = [{"lat":loc[0], "lng":loc[1], "infobox": loc[2]} for loc in locations],
-            fit_markers_to_bounds = True,
-            style="width:100%; height:100%;"
+                identifier="view-side",
+                lat=33.4227198,
+                lng= -111.79821229999999,
+                markers = [{"lat":loc[0], "lng":loc[1], "infobox": loc[2]} for loc in locations],
+                fit_markers_to_bounds = True,
+                style="width:100%; height:100%;"
             )
-            
-
+        
             return render_template('map.html', names = names ,map=mymap,destination = destination,restaurant = restaurant)
-        except:
-            return render_template('base.html')
+        # #except:
+        #     return render_template('error.html')
+
+
+@app.route('/map2', methods=['POST', 'GET'])
+
+def get_information2():
+
+
+    if request.method == 'POST':
+        address=request.form['address']
+        departure = request.form['departure']
+        destination = "mesa"
+        query = pd.read_csv("csv/" + departure +'.csv')
+        query = query[query["address"] == address]
+        restaurant = query.name.values[0]
+
+        most_similar_df = get_top_ten(query,departure,destination)
+
+        
+        names =  most_similar_df.name.tolist()
+        latitudes = most_similar_df.latitude.tolist()
+        longitudes = most_similar_df.longitude.tolist()
+        locations = list(zip(latitudes,longitudes,names))
+        
+
+
+        mymap = Map(
+        identifier="view-side",
+        lat=33.4227198,
+        lng= -111.79821229999999,
+        markers = [{"lat":loc[0], "lng":loc[1], "infobox": loc[2]} for loc in locations],
+        fit_markers_to_bounds = True,
+        style="width:100%; height:100%;"
+        )
+        
+
+        return render_template('map.html', names = names ,map=mymap,destination = destination,restaurant = restaurant)
+        
 
 
     else:
         return render_template('base.html', form=form,map = mymap)
 
 if __name__ == '__main__':
-=======
-from flask import Flask, render_template, flash, request, jsonify
-from wtforms import Form, TextField, TextAreaField, validators, StringField, SubmitField
-from config import Config
-from forms import LoginForm,ReusableForm
-
-import pickle
-import pandas as pd
-from scipy.stats import entropy
-import numpy as np
-
-from flask_googlemaps import GoogleMaps
-from flask_googlemaps import Map, icons 
-
-
-# create the application object
-app = Flask(__name__)
-app.config.from_object(Config)
-app.config['GOOGLEMAPS_KEY'] = "  <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCjmRF-E30xmEShihkMA8thF9nGiLqUtNY&callback=initMap"type="text/javascript"></script>" 
-GoogleMaps(app)
-
-mesa_lda = pickle.load(open("mesa_lda.pkl","rb"))
-mesa_tfidf = pickle.load(open("mesa_tfidf.pkl","rb"))
-mesa_dictionary = pickle.load(open("mesa_dictionary.pkl","rb"))
-mesa_doc_topic_dist = pickle.load(open("mesa_doc_topic_dist.pkl","rb"))
-
-
-
-def jensen_shannon(query, matrix):
-    """
-    This function implements a Jensen-Shannon similarity
-    between the input query (an LDA topic distribution for a document)
-    and the entire corpus of topic distributions.
-    It returns an array of length M where M is the number of documents in the corpus
-    """
-    # lets keep with the p,q notation above
-    p = query[None,:].T # take transpose
-    q = matrix.T # transpose matrix
-    m = 0.5*(p + q)
-    return np.sqrt(0.5*(entropy(p,m) + entropy(q,m)))
-
-def get_most_similar_documents(query,matrix,k=10):
-    """
-    This function implements the Jensen-Shannon distance above
-    and retruns the top k indices of the smallest jensen shannon distances
-    """
-    sims = jensen_shannon(query,matrix) # list of jensen shannon distances
-    return sims.argsort()[:k] # the top k positional index of the smallest Jensen Shannon distances
-
-
-def get_top_ten(departure,destination,restaurant):
-    query = pd.read_csv(departure +'_cleaned.csv')
-    destination = pd.read_csv("reviews"+"_"+destination+ ".csv")
-    df = destination.head()
-
-    query = query[query["name"] == restaurant]
-    #get the ids of the most similar businesses
-    new_bow = mesa_dictionary.doc2bow(eval(query.iloc[0,3]))
-    new_doc_distribution = np.array([tup[1] for tup in mesa_lda.get_document_topics(bow=mesa_tfidf[new_bow])])
-    most_sim_ids = get_most_similar_documents(new_doc_distribution,mesa_doc_topic_dist)
-
-    # print the results
-    most_similar_df = destination[destination.index.isin(most_sim_ids)]
-    return(most_similar_df)
-
-@app.route('/', methods=['GET', 'POST'])
-def login():
-    return render_template('index2.html')
-
-@app.route('/test')
-def map():
-    return render_template('results.html')
-
-
-@app.route("/map", methods=['POST', 'GET'])
-def get_information():
-    form = ReusableForm(request.form)
-    print (form.errors)
-
-    mymap = Map(
-        identifier="view-side",
-        lat=33.4227198,
-        lng= -111.79821229999999,
-        markers=[(33.4227198, -111.79821229999999),
- (33.383889224200004, -111.85933446930001),
- (33.3840839, -111.84369640000001),
- (33.374984999999995, -111.68716299999998),
- (33.415843699999996, -111.6688207)],
-        style="width:100%; height:400px;"
-     )
-
-    if request.method == 'POST':
-        departure=request.form['departure']
-        destination=request.form['destination']
-        restaurant=request.form['restaurant']
-
-        most_similar_df = get_top_ten(departure,destination,restaurant)[["name"]]
-        
-        return render_template('simple.html',  tables=[most_similar_df.to_html(classes='data')], titles=most_similar_df.columns.values,map=mymap)
-        #return render_template('results.html',  search_term=search_term, departure=departure, destination=destination)
-    else:
-        return render_template('index.html', form=form,map = mymap)
-
-# Run the app from here
-if __name__ == '__main__':
->>>>>>> 02ec273fb8f0f05893e4159e65801f35af682a77
     app.run(debug = True)
